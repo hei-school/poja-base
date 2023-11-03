d95bb6c1fc67b5a4182305a2cc547154b490c836
Lou Andria <lou@hei.school>
2023-11-03 09:47:58 +0100

    cli=4c0b297

--- src/main/java/com/company/base/MailboxEventHandler.java
+++ src/main/java/com/company/base/MailboxEventHandler.java
@@ -5,16 +5,29 @@ import com.company.base.endpoint.event.EventConsumer;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Collections;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
+import static java.util.Collections.singletonMap;
 import static com.company.base.endpoint.event.EventConsumer.toAcknowledgeableTypedEvent;
 
 @Slf4j
 public class MailboxEventHandler implements RequestHandler<SQSEvent, String> {
+
+  private static int anAvailableRandomPort() {
+    try(ServerSocket serverSocket = new ServerSocket(0)) {
+      return serverSocket.getLocalPort();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   @Override
   public String handleRequest(SQSEvent event, Context context) {
@@ -34,6 +47,8 @@ public class MailboxEventHandler implements RequestHandler<SQSEvent, String> {
   }
 
   private ConfigurableApplicationContext applicationContext(String... args) {
-    return SpringApplication.run(PojaApplication.class, args);
+    SpringApplication application = new SpringApplication(MailboxEventHandler.class);
+    application.setDefaultProperties(singletonMap("server.port", anAvailableRandomPort()));
+    return application.run(args);
   }
 }
