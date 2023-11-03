936e99e034d7f5a5bda578321c144591ad48d7b0
Lou Andria <lou@hei.school>
2023-11-03 11:24:28 +0100

    cli=d2caa23

--- src/main/java/com/company/base/MailboxEventHandler.java
+++ src/main/java/com/company/base/MailboxEventHandler.java
@@ -3,16 +3,16 @@ package com.company.base;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
-import com.company.base.endpoint.event.EventConf;
-import com.company.base.endpoint.event.EventConsumer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import com.company.base.endpoint.event.EventConf;
+import com.company.base.endpoint.event.EventConsumer;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 import java.util.List;
-import java.util.Map;
 
+import static java.util.Collections.singletonMap;
 import static com.company.base.endpoint.event.EventConsumer.toAcknowledgeableTypedEvent;
 
 @Slf4j
@@ -20,7 +20,7 @@ public class MailboxEventHandler implements RequestHandler<SQSEvent, String> {
 
   @Override
   public String handleRequest(SQSEvent event, Context context) {
-    log.info("Received: event={}, context={}", event, context);
+    log.info("Received: event={}, awsReqId={}", event, context.getAwsRequestId());
     List<SQSEvent.SQSMessage> messages = event.getRecords();
     log.info("SQS messages: {}", messages);
 
@@ -32,19 +32,12 @@ public class MailboxEventHandler implements RequestHandler<SQSEvent, String> {
     eventConsumer.accept(toAcknowledgeableTypedEvent(eventConf, sqsClient, messages));
 
     applicationContext.close();
-    return "{message: ok}";
+    return "ok";
   }
 
-  private ConfigurableApplicationContext applicationContext() {
-    log.info("DATABASE_URL");
-    log.info(System.getenv("DATABASE_URL"));
+  private ConfigurableApplicationContext applicationContext(String... args) {
     SpringApplication application = new SpringApplication(PojaApplication.class);
-    application.setDefaultProperties(Map.of(
-        "spring.main.web-application-type", "none",
-        "spring.datasource.url", System.getenv("DATABASE_URL"),
-        "spring.datasource.username", System.getenv("DATABASE_USERNAME"),
-        "spring.datasource.password", System.getenv("DATABASE_PASSWORD")
-    ));
-    return application.run();
+    application.setDefaultProperties(singletonMap("spring.main.web-application-type", "none"));
+    return application.run(args);
   }
 }
--- src/main/java/com/company/base/PojaApplication.java
+++ src/main/java/com/company/base/PojaApplication.java
@@ -1,25 +1,14 @@
 package com.company.base;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @SpringBootApplication
 public class PojaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(PojaApplication.class, args);
-  }
-
-  @Bean
-  ApplicationRunner applicationRunner(Environment environment) {
-    return args -> {
-      log.info("env=" + environment);
-    };
   }
 
 }
