62842a8fd066bb976a3b5c7795068707d6966fa6
Lou Andria <lou@hei.school>
2023-11-14 18:47:13 +0100

    cli=62b30b6eb

--- src/test/java/com/company/base/conf/EnvConf.java
+++
@@ -1,9 +0,0 @@
-package com.company.base.conf;
-
-import org.springframework.test.context.DynamicPropertyRegistry;
-
-public class EnvConf {
-
-  void configureProperties(DynamicPropertyRegistry registry) {
-  }
-}
