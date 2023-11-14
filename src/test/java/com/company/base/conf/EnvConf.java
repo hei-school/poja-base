9351c8806aead83775faa7175dc7bd270d87be02
Lou Andria <lou@hei.school>
2023-11-14 18:32:21 +0100

    fix: add EnvConf

---
+++ src/test/java/com/company/base/conf/EnvConf.java
@@ -0,0 +1,9 @@
+package com.company.base.conf;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+
+public class EnvConf {
+
+  void configureProperties(DynamicPropertyRegistry registry) {
+  }
+}
