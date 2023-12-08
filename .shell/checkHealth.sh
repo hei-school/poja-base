68de6de18b5d6047a84617e78a3002c9a8d97f98
Lou Andria <lou@hei.school>
2023-12-08 14:28:55 +0100

    fixup

--- .shell/checkHealth.sh
+++ .shell/checkHealth.sh
@@ -1,4 +1,4 @@
 export API_URL_SSM="`aws ssm get-parameter --name /poja-base/${{ github.ref_name }}/api/url`"
 sudo apt-get install jq
 export API_URL=`echo $API_URL_SSM | jq -r '.Parameter.Value'`
-curl --fail "$API_URL"$1
+curl --fail "$API_URL$1"
