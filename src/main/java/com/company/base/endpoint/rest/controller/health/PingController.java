bbe9701b04beac227a20c57cac2d5f7ba6d33acc
Lou Andria <lou@hei.school>
2023-12-11 13:47:23 +0100

    feat: mail

---
+++ src/main/java/com/company/base/endpoint/rest/controller/health/PingController.java
@@ -0,0 +1,28 @@
+package com.company.base.endpoint.rest.controller.health;
+
+import com.company.base.PojaGenerated;
+import com.company.base.repository.DummyRepository;
+import com.company.base.repository.DummyUuidRepository;
+import lombok.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@PojaGenerated
+@RestController
+@Value
+public class PingController {
+
+  DummyRepository dummyRepository;
+  DummyUuidRepository dummyUuidRepository;
+
+  public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
+  public static final ResponseEntity<String> KO =
+      new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
+
+  @GetMapping("/ping")
+  public String ping() {
+    return "pong";
+  }
+}
