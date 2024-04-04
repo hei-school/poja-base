53e627cf0909c7c9d64503d5ada41186559227bb
onitsiky <hei.onitsiky@gmail.com>
2024-04-04 13:35:53 +0300

    poja-upgrade: 12.6.0

--- src/main/java/com/company/base/endpoint/rest/controller/MultipartFileController.java
+++ src/main/java/com/company/base/endpoint/rest/controller/MultipartFileController.java
@@ -18,29 +18,33 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @Slf4j
 public class MultipartFileController {
-	//shows we can handle multipart files
-	@PutMapping(value = "/multipart-upload", consumes = MULTIPART_FORM_DATA_VALUE)
-	public ResponseEntity<String> uploadMultipart(
-		@RequestHeader
-		HttpHeaders httpHeaders,
-		@RequestPart(name = "payload") MultipartFile multipartFile
-	) {
-		log.info("headers {}", httpHeaders);
-		//if (true) {
-		//MultipartFile multipartFile = (MultipartFile) multipart;
-		log.info("multipart file details original filename = {} sizeInBytes = {}", multipartFile.getOriginalFilename(), multipartFile.getSize());
-		try {
-			var fileToUpload = createTempFile(multipartFile.getOriginalFilename(), null);
-			multipartFile.transferTo(fileToUpload);
-			log.info("saved  file details original filename = {} sizeInBytes = {}", fileToUpload.getName(), fileToUpload.length());
-			return OK;
-		} catch (IOException e) {
-			log.info("exception");
-			return KO;
-		}
-		//} else {
-//			log.info("multipart {}", multipart);
-//			return KO;
-//		}
-	}
+  // shows we can handle multipart files
+  @PutMapping(value = "/multipart-upload", consumes = MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<String> uploadMultipart(
+      @RequestHeader HttpHeaders httpHeaders,
+      @RequestPart(name = "payload") MultipartFile multipartFile) {
+    log.info("headers {}", httpHeaders);
+    // if (true) {
+    // MultipartFile multipartFile = (MultipartFile) multipart;
+    log.info(
+        "multipart file details original filename = {} sizeInBytes = {}",
+        multipartFile.getOriginalFilename(),
+        multipartFile.getSize());
+    try {
+      var fileToUpload = createTempFile(multipartFile.getOriginalFilename(), null);
+      multipartFile.transferTo(fileToUpload);
+      log.info(
+          "saved  file details original filename = {} sizeInBytes = {}",
+          fileToUpload.getName(),
+          fileToUpload.length());
+      return OK;
+    } catch (IOException e) {
+      log.info("exception");
+      return KO;
+    }
+    // } else {
+    //			log.info("multipart {}", multipart);
+    //			return KO;
+    //		}
+  }
 }
--- src/main/java/com/company/base/endpoint/rest/security/SecurityConf.java
+++ src/main/java/com/company/base/endpoint/rest/security/SecurityConf.java
@@ -30,21 +30,21 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 @EnableWebSecurity
 public class SecurityConf {
 
-	public static final String AUTHORIZATION_HEADER = "Authorization";
-	private final AuthProvider authProvider;
-	private final HandlerExceptionResolver exceptionResolver;
+  public static final String AUTHORIZATION_HEADER = "Authorization";
+  private final AuthProvider authProvider;
+  private final HandlerExceptionResolver exceptionResolver;
 
-	public SecurityConf(
-		AuthProvider authProvider,
-		// InternalToExternalErrorHandler behind
-		@Qualifier("handlerExceptionResolver") HandlerExceptionResolver exceptionResolver) {
-		this.authProvider = authProvider;
-		this.exceptionResolver = exceptionResolver;
-	}
+  public SecurityConf(
+      AuthProvider authProvider,
+      // InternalToExternalErrorHandler behind
+      @Qualifier("handlerExceptionResolver") HandlerExceptionResolver exceptionResolver) {
+    this.authProvider = authProvider;
+    this.exceptionResolver = exceptionResolver;
+  }
 
-	@Bean
-	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		// @formatter:off
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    // @formatter:off
     http.exceptionHandling(
             (exceptionHandler) ->
                 exceptionHandler
@@ -70,7 +70,7 @@ public class SecurityConf {
                     new OrRequestMatcher(
                         new AntPathRequestMatcher("/ping", GET.toString()),
                         new AntPathRequestMatcher("/health/**"),
-												new AntPathRequestMatcher("/multipart-upload", PUT.toString())))),
+                        new AntPathRequestMatcher("/multipart-upload", PUT.toString())))),
             AnonymousAuthenticationFilter.class)
         .authorizeHttpRequests(
             (authorize) ->
@@ -79,8 +79,8 @@ public class SecurityConf {
                     .permitAll()
                     .requestMatchers("/health/**")
                     .permitAll()
-                  	.requestMatchers(PUT, "/multipart-upload")
-										.permitAll()
+                    .requestMatchers(PUT, "/multipart-upload")
+                    .permitAll()
                     .requestMatchers(POST, "/secret")
                     .hasRole(WHISTLEBLOWER.getRole())
                     .anyRequest()
