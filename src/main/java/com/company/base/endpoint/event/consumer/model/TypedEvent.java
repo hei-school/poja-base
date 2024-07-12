fa337f8f71ddb681e5172dbd70302ad07d73d261
Onitsiky <hei.onitsiky@gmail.com>
2024-07-12 11:48:56 +0300

    feat: no async stack

--- src/main/java/com/company/base/endpoint/event/consumer/model/TypedEvent.java
+++
@@ -1,7 +0,0 @@
-package com.company.base.endpoint.event.consumer.model;
-
-import com.company.base.PojaGenerated;
-import com.company.base.endpoint.event.model.PojaEvent;
-
-@PojaGenerated
-public record TypedEvent(String typeName, PojaEvent payload) {}
